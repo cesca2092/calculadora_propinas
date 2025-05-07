b0309b16e2ae08da64a537266856ba8311d2fcf5
cesar <cesca2092@gmail.com>
2025-05-07 10:57:41 -0500

    agregar item

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,13 +1,25 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { MenuItem, OrderItem } from "../types"
 
 export default function useOrder() {
 
-  const [useOrder, setUseOrder] = useState<OrderItem[]>([])
-  const [total, setTotal] = useState(0)
+  const [order, setOrder] = useState<OrderItem[]>([])
 
+  const addItem = (item: MenuItem) => {
 
+    const itemExists = order.findIndex(orderItem => orderItem.id === item.id)
+    if (itemExists === -1) {
+      const newItem = { ...item, quantity: 1 }
+      setOrder([...order, newItem])
+    } else {
+      const copyOrder = order.map(el => el)
+      copyOrder[itemExists].quantity += 1
+      setOrder(copyOrder)
+    }
+
+  }
   return {
-
+    order,
+    addItem
   }
 }
